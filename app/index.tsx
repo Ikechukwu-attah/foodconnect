2d927e242f48403081f7108416c64775b8eb63b3
Ikechukwu-attah <attahikechukwu@gmail.com>
2024-08-01 05:33:27 -0400

    adds intro text to first screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,32 @@
 import { Link } from "expo-router";
-import { ScrollView, Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView className="bg-primary-dark h-full">
-      <ScrollView contentContainerStyle={{}}></ScrollView>
+    <SafeAreaView className="h-full bg-background-dark">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="items-center justify-center w-full h-full gap-8 px-4">
+          <Image
+            className="w-[130px] h-[84px]"
+            source={require("../assets/images/foodlogo.png")}
+            resizeMode="contain"
+          />
+
+          <Image
+            className="max-w-[300px] w-full h-[200px]"
+            source={require("../assets/images/eating.jpg")}
+            resizeMode="contain"
+          />
+
+          <View className="relative ">
+            <Text className="text-3xl font-bold text-center text-white ">
+              Discover Endless Food bank with {""}
+              <Text className="text-primary">FoodConnect</Text>
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
